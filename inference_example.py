ee7ed7748398d306a2e296bda6ce2b3c0ecdc688
ToPro17 <109027142+ToPro17@users.noreply.github.com>
2026-01-21 13:57:23 +0100

    Update inference_example.py

--- inference_example.py
+++ inference_example.py
@@ -1,63 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 import torch
 import importlib
 from pathlib import Path
 
-# Import model and functions
-from jeffrey_code import hydraulic_conductivity_field, solve_darcy_flow, source_function
-
-
-model_id = "cnn12c"  # change this to use different models
-postfix = "_lr6e-5"
-
-model_file = importlib.import_module(f"models.{model_id}.model")  # this line imports the right model and training settings
-
-# Device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Using device:", device)
-
-# Checkpoint folder
-out_dir = Path("models") / model_id
-out_dir.mkdir(exist_ok=True)
-
-# Load model checkpoint
-ckpt_path = out_dir / f"model_last{postfix}.pt"
-ckpt = torch.load(ckpt_path, map_location=device)
-
-model = model_file.Model().to(device)
-model.load_state_dict(ckpt["model_state_dict"])
-model.eval()
-
-logk_center = ckpt["norm"]["logk_center"]
-h_mean = ckpt["norm"]["h_mean"]
-h_std = ckpt["norm"]["h_std"]
-
-
-# Settings
-n = 60
-BATCH_SIZE = 16
-PLOT_H = True
 np.random.seed(42)
-seeds = np.random.choice(range(10000), size=BATCH_SIZE, replace=False)
-f = source_function(n)
+
+from jeffrey_code import hydraulic_conductivity_field, source_function
+
+class Predictor_example:
+    def __init__(self, model_id, postfix):
+
+        model_file = importlib.import_module(f"models.{model_id}.model")  # this line imports the right model and training settings
+
+        # Device
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print("Using device:", self.device)
+
+        # Checkpoint folder
+        out_dir = Path("models") / model_id
+        out_dir.mkdir(exist_ok=True)
+
+        # Load model checkpoint
+        ckpt_path = out_dir / f"model_last{postfix}.pt"
+        ckpt = torch.load(ckpt_path, map_location=self.device)
+
+        self.model = model_file.Model().to(self.device)
+        self.model.load_state_dict(ckpt["model_state_dict"])
+        self.model.eval()
+
+        self.logk_center = ckpt["norm"]["logk_center"]
+        self.h_mean = ckpt["norm"]["h_mean"]
+        self.h_std = ckpt["norm"]["h_std"]
+
+    def predict(self, kappa):
+        # Predicted hydraulic head
+        x_input = torch.tensor(np.log(kappa) - self.logk_center, dtype=torch.float32).view((-1, 1, 60, 60)).to(self.device)
+
+        with torch.no_grad():
+            h_pred_norm = self.model(x_input)
+        return h_pred_norm.cpu().numpy()[0] * self.h_std + self.h_mean
 
 
-# Generate random hydraulic conductivity field; replace this with the input
-kappa = np.empty((BATCH_SIZE, 3600))
-for j, seed in enumerate(seeds):
-    kappa[j] = hydraulic_conductivity_field(n, seed)
+def random_input(n=60):
+    BATCH_SIZE = 16
+    seeds = np.random.choice(range(10000), size=BATCH_SIZE, replace=False)
+    # Generate random hydraulic conductivity field; replace this with the input
+    kappa = np.empty((BATCH_SIZE, 3600))
+    for j, seed in enumerate(seeds):
+        kappa[j] = hydraulic_conductivity_field(n, seed)
+    return kappa
 
-# Predicted hydraulic head
-x_input = torch.tensor(np.log(kappa) - logk_center, dtype=torch.float32).view((-1, 1, 60, 60)).to(device)
+def main():
+    # make a random input
+    kappa = random_input()
 
-with torch.no_grad():
-    h_pred_norm = model(x_input)
+    # make the predictor
+    predictor = Predictor_example("cnn12c", "_lr6e-5")
+    # this is the inference
+    h_pred = predictor.predict(kappa)
 
-if PLOT_H:
-    h_pred = h_pred_norm.cpu().numpy()[0] * h_std + h_mean
-
+    # show results
     plt.imshow(h_pred, cmap='hot_r', origin='lower', interpolation='none')
     plt.title(f'Predicted h')
     plt.xlabel('x (km)')
@@ -66,4 +69,5 @@ if PLOT_H:
     plt.tight_layout()
     plt.show()
 
-
+if __name__ == "__main__":
+    main()
